367de19c27bea654b59fb9bc3c50d80873ea8793
bravech <cgp246@gmail.com>
2020-06-06 22:10:29 -0400

    Initial structure and headers

---
+++ CHIP-8/Chip8Emu.cpp
@@ -0,0 +1,3 @@
+#include "Chip8Emu.h"
+Chip8Emu::Chip8Emu(std::string rom_path) {};
+Chip8Emu::~Chip8Emu() {};
---
+++ CHIP-8/Chip8Emu.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream>
+class Chip8Emu
+{
+public:
+	int main();
+	int init();
+	Chip8Emu(std::string rom_path);
+	~Chip8Emu();
+private:
+	uint8_t mem[0x1000]; // Memory of the CHIP-8
+	uint8_t v[16]; // Registers
+	uint16_t I; // Address register
+	uint8_t* code;
+
+	sf::Uint8 screen[64 * 32];
+	sf::RenderWindow window;
+	sf::Event event;
+};
+
