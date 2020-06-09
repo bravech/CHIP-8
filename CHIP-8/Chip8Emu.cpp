90d89acd10ea7b67df0050113a1bb90f4b42bdfe
bravech <cgp246@gmail.com>
2020-06-09 16:25:30 -0400

    Added some opcodes and timer threads

--- CHIP-8/Chip8Emu.cpp
+++ CHIP-8/Chip8Emu.cpp
@@ -37,7 +37,7 @@ void Chip8Emu::execOpcode() {
 		}
 		break;
 	case 5:
-		if (v[nibbles[1]] != v[nibbles[2]]) {
+		if (v[nibbles[1]] == v[nibbles[2]]) {
 			pc += 2;
 		}
 		break;
@@ -58,15 +58,67 @@ void Chip8Emu::execOpcode() {
 		case 2:
 			v[nibbles[1]] = v[nibbles[1]] & v[nibbles[2]];
 			break;
+		case 3:
+			v[nibbles[1]] = v[nibbles[1]] ^ v[nibbles[2]];
+			break;
+		case 4:
+			v[nibbles[1]] = v[nibbles[1]] + v[nibbles[2]];
+			break;
+		case 5:
+			v[nibbles[1]] = v[nibbles[1]] - v[nibbles[2]];
+			break;
+		case 6:
+			v[nibbles[1]] = v[nibbles[1]] >> 1; //TODO: Double check this is proper, may use VY also
+			break;
+		case 7:
+			v[nibbles[1]] = v[nibbles[2]] - v[nibbles[1]];
+			break;
+		case 0xe:
+			v[nibbles[1]] = v[nibbles[1]] << 1; //TODO: Double check this is proper, may use VY also
+			break;
 		}
 		break;
+	case 9:
+		if (v[nibbles[1]] != v[nibbles[2]]) {
+			pc += 2;
+		}
+		break;
+	case 0xa:
+		I = (nibbles[1] << 8) + code[pc + 1];
+		break;
+	case 0xb:
+		pc = v[0] + (nibbles[1] << 8) + code[pc + 1];
+		break;
+	case 0xc:
+		v[nibbles[1]] = (rand() & 0xff) & code[pc + 1];
+		break;
+	case 0xd:
+		// TODO: Implement graphics
+		break;
+	case 0xe:
+		// TODO: Implement input / keycodes
+		break;
+	case 0xf:
+		switch (nibbles[3]) {
+		case 7:
+			break;
+		case 0xa:
+			break;
+		}
+		break;
+
+
 	default:
-		std::cout << "WARN: Invalid Opcode Detected! " << std::hex << (int)code[pc] << " " << (int)code[pc + 1] << std::endl;
+		//std::cout << "WARN: Invalid Opcode Detected! " << std::hex << (int)code[pc] << " " << (int)code[pc + 1] << std::endl;
+		break;
+		
 	}
 	pc += 2;
 }
 
 int Chip8Emu::run() {
+	std::thread timerThread(&Chip8Emu::decrementTimers, this);
+
 	while (window.isOpen()) {
 		execOpcode();
 		while (window.pollEvent(event)) {
@@ -78,4 +130,29 @@ int Chip8Emu::run() {
 		window.display();
 	}
 	return 0;
+}
+
+void Chip8Emu::decrementTimers() {
+	using frames = std::chrono::duration < int64_t, std::ratio<1, TIMER_HZ> >;
+	auto nextFrame = std::chrono::system_clock::now() + frames{ 0 };
+	auto lastFrame = nextFrame - frames{ 1 };
+
+	while (true) {
+		soundTimerMutex.lock();
+		if (soundTimer > 0) {
+			soundTimer--;
+		}
+		soundTimerMutex.unlock();
+		delayTimerMutex.lock();
+		if (delayTimer > 0) {
+			delayTimer--;
+		}
+		delayTimerMutex.unlock();
+		std::this_thread::sleep_until(nextFrame);
+		if (DEBUG) {
+			std::cout << "Timer clock: " << std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now() - lastFrame).count() << std::endl;
+		}
+		lastFrame = nextFrame;
+		nextFrame = nextFrame + frames{ 1 };
+	}
 }
--- CHIP-8/Chip8Emu.h
+++ CHIP-8/Chip8Emu.h
@@ -4,7 +4,15 @@
 #include <iostream>
 #include <fstream>
 #include <ios>
+#include <stdlib.h>
+#include <thread>
+#include <chrono>
+#include <mutex>
 #define WIN_SIZE 400
+#define TIMER_HZ 60
+#define CPU_HZ 500
+#define DRAW_HZ 60
+#define DEBUG true
 
 class Chip8Emu
 {
@@ -13,6 +21,7 @@ public:
 	int run();
 	void execOpcode();
 	Chip8Emu(std::string rom_path);
+	void decrementTimers();
 	~Chip8Emu();
 private:
 	uint8_t mem[0x1000]; // Memory of the CHIP-8
@@ -20,6 +29,12 @@ private:
 	uint16_t I; // Address register
 	uint8_t* code;
 	uint16_t pc;
+	uint8_t soundTimer;
+	std::mutex soundTimerMutex;
+	uint8_t delayTimer;
+	std::mutex delayTimerMutex;
+	
+
 
 	sf::Uint8 screen[64 * 32];
 	sf::RenderWindow window;
